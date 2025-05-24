162c5e23342f613092856f688df17c6be5204b7f
Simon Fredsted <simon@fredsted.me>
2025-05-24 13:49:43 +0200

    Add --query flag to process past requests

--- commands/forward.js
+++ commands/forward.js
@@ -2,7 +2,7 @@ import fetch, {FormData} from "node-fetch";
 import listen from "./lib/listen.js";
 import replaceVariables from "./lib/replace-variables.js";
 import log from "./lib/log.js";
-import {setApiKey, setResponse, updateTokenListen} from "./lib/api.js";
+import {scanRequests, setApiKey, setResponse, updateTokenListen} from "./lib/api.js";
 
 const getTargetPath = function (url) {
     // We only want the `/a/b/c` part:
@@ -18,13 +18,89 @@ const getTargetPath = function (url) {
     return pathMatch ? pathMatch[1] : '';
 }
 
+const forward = (tokenId, request, variables, target, keepUrl, listenSeconds) => {
+    target = replaceVariables(target, variables)
+    if (!keepUrl) {
+        const query = request.query !== null
+            ? '?' + new URLSearchParams(request.query).toString()
+            : '';
+
+        target = target + getTargetPath(request.url) + query;
+    }
+
+    let options = {
+        method: request.method,
+        headers: request.headers,
+        body: null,
+    };
+
+    if (listenSeconds > 0) {
+        // Enough time to clear token listen property when command exits.
+        options['signal'] = AbortSignal.timeout(listenSeconds * 1000);
+    }
+
+    const removeHeaders = [
+        'host',
+        'content-length'
+    ]
+
+    for (let headerName of removeHeaders) {
+        if (headerName in options.headers) {
+            delete options.headers[headerName]
+        }
+    }
+
+    if (request.method !== 'GET' && request.method !== 'HEAD') {
+        options['body'] = request.content
+
+        // Handle serialized multipart requests
+        if (options['body'] === '' && request.request) {
+            options['body'] = new FormData();
+            // node-fetch generates a new Content-Type header
+            delete options.headers['content-type'];
+
+            for (const formFieldName in request.request) {
+                options['body'].append(formFieldName, request.request[formFieldName])
+            }
+        }
+    }
+
+    fetch(target, options)
+        .then(async (res) => {
+            log.info({
+                msg: 'Forwarded request',
+                url: res.url,
+                status: res.status,
+            });
+            if (listenSeconds > 0) {
+                await setResponse(
+                    tokenId,
+                    request.uuid,
+                    res.status,
+                    res.arrayBuffer(),
+                    res.headers.raw(),
+                    listenSeconds * 1000
+                )
+            }
+        })
+        .catch((err) => {
+            log.error({
+                msg: 'Error forwarding request',
+                err,
+            })
+        })
+}
+
 export default async (argv) => {
     if (!argv.token && !process.env.WH_TOKEN) {
         throw new Error('Please specify a token (--token)')
     }
     const tokenId = argv.token ?? process.env.WH_TOKEN;
     const apiKey = argv['api-key'] ?? process.env.WH_API_KEY;
+    const searchQuery = argv['query'] ?? process.env.WH_QUERY;
     const listenSeconds = argv['listen-timeout'] ?? process.env.WH_LISTEN_TIMEOUT ?? 5;
+    const keepUrl = argv['keep-url'] ?? false;
+    const target = argv.target ?? process.env.WH_TARGET ?? 'https://localhost';
 
     setApiKey(apiKey);
 
@@ -40,82 +116,19 @@ export default async (argv) => {
     process.on('exit', clearTokenListen)
     process.on('SIGINT', clearTokenListen)
 
-    listen(
-        tokenId,
-        apiKey,
-        (data) => {
-            let target = replaceVariables(argv.target ?? process.env.WH_TARGET ?? 'https://localhost', data.variables)
-
-            if (!argv['keep-url']) {
-                let path = '';
-                const query = data.request.query !== null
-                    ? '?' + new URLSearchParams(data.request.query).toString()
-                    : '';
-
-                target = target + getTargetPath(data.request.url) + query;
+    if (searchQuery) {
+        // Loop through existing requests if search query specified
+        await scanRequests(tokenId, searchQuery, (request) => {
+            forward(tokenId, request, {}, target, keepUrl, 0)
+        })
+    } else {
+        // Listen for new requests via WebSocket
+        listen(
+            tokenId,
+            apiKey,
+            (data) => {
+                forward(tokenId, data.request, data.variables, target, keepUrl, listenSeconds)
             }
-
-            let options = {
-                method: data.request.method,
-                headers: data.request.headers,
-                body: null,
-            };
-
-            if (listenSeconds > 0) {
-                // Enough time to clear token listen property when command exits.
-                options['signal'] = AbortSignal.timeout(listenSeconds * 1000);
-            }
-
-            const removeHeaders = [
-                'host',
-                'content-length'
-            ]
-
-            for (let headerName of removeHeaders) {
-                if (headerName in options.headers) {
-                    delete options.headers[headerName]
-                }
-            }
-
-            if (data.request.method !== 'GET' && data.request.method !== 'HEAD') {
-                options['body'] = data.request.content
-
-                // Handle serialized multipart requests
-                if (options['body'] === '' && data.request.request) {
-                    options['body'] = new FormData();
-                    // node-fetch generates a new Content-Type header
-                    delete options.headers['content-type'];
-
-                    for (const formFieldName in data.request.request) {
-                        options['body'].append(formFieldName, data.request.request[formFieldName])
-                    }
-                }
-            }
-
-            fetch(target, options)
-                .then(async (res) => {
-                    log.info({
-                        msg: 'Forwarded incoming request',
-                        url: res.url,
-                        status: res.status,
-                    });
-                    if (listenSeconds > 0) {
-                        await setResponse(
-                            tokenId,
-                            data.request.uuid,
-                            res.status,
-                            res.arrayBuffer(),
-                            res.headers.raw(),
-                            listenSeconds * 1000
-                        )
-                    }
-                })
-                .catch((err) => {
-                    log.error({
-                        msg: 'Error forwarding incoming request',
-                        err,
-                    })
-                })
-        }
-    )
+        )
+    }
 }
