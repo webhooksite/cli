f45be5962a00dc87d9b84ac2c522f85b20c0584d
Simon Fredsted <simon@fredsted.me>
2025-05-27 12:42:48 +0200

    Add --query to `exec`

--- commands/exec.js
+++ commands/exec.js
@@ -2,6 +2,25 @@ import listen from "./lib/listen.js";
 import replaceVariables from "./lib/replace-variables.js";
 import log from "./lib/log.js";
 import child_process from "child_process";
+import {scanRequests, setApiKey} from "./lib/api.js";
+
+const exec = (tokenId, request, variables, command) => {
+    replaceVariables(command, variables)
+
+    child_process.exec(
+        command,
+        (error, stdout, stderr) => {
+            log.info({
+                'msg': 'Command was executed',
+                command,
+                error,
+                stderr,
+                stdout,
+            });
+        }
+    )
+}
+
 
 export default async (argv) => {
     if (!argv.token && !process.env.WH_TOKEN) {
@@ -10,25 +29,25 @@ export default async (argv) => {
     if (!argv.command && !process.env.WH_COMMAND) {
         throw new Error('Please specify a command (--command)')
     }
+    const searchQuery = argv['query'] ?? process.env.WH_QUERY;
+    const tokenId = argv.token ?? process.env.WH_TOKEN;
+    const command = argv.command ?? process.env.WH_COMMAND;
+    const apiKey = argv['api-key'] ?? process.env.WH_API_KEY;
 
-    listen(
-        argv.token ?? process.env.WH_TOKEN,
-        argv['api-key'] ?? process.env.WH_API_KEY,
-        (data) => {
-            const command = replaceVariables(argv.command ?? process.env.WH_COMMAND, data.runtime_variables)
+    setApiKey(apiKey);
 
-            child_process.exec(
-                command,
-                (error, stdout, stderr) => {
-                    log.info({
-                        'msg': 'Command was executed',
-                        command,
-                        error,
-                        stderr,
-                        stdout,
-                    });
-                }
-            )
-        }
-    )
+    if (searchQuery) {
+        await scanRequests(tokenId, searchQuery, (request) => {
+            exec(tokenId, request, {}, command)
+        })
+    } else {
+        listen(
+            tokenId,
+            argv['api-key'] ?? process.env.WH_API_KEY,
+            (data) => {
+                exec(tokenId, data.request, data.runtime_variables, command);
+            }
+        )
+    }
+
 }
