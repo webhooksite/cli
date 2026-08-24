44762a9f5b4a01b179848d1cabf3e030b2d77252
Simon Fredsted <simon@fredsted.me>
2026-08-24 13:45:23 +0200

    Update version and package-lock

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ loadConfig();
 
 const argv = minimist(process.argv.slice(2));
 const command = argv['_'][0];
-const version = '0.3.1';
+const version = '0.3.2';
 
 switch (command) {
     case 'forward': {
